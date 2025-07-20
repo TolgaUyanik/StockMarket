cff80a267dc145e0b668d2fafa8be6dccccbe773
TolgaUyanik <uyaniktolga123@gmail.com>
2025-07-21 00:55:07 +0300

    Backtrader added

--- Backtesting_Tolga.py
+++ Backtesting_Tolga.py
@@ -17,29 +17,30 @@ Coin = ["BTC-USD", "ETH-USD", "USDT-USD", "BNB-USD", "USDC-USD", "XRP-USD", "DOG
 bist = ['A1CAP.IS','ACSEL.IS','ADESE.IS','ADGYO.IS','AEFES.IS','AFYON.IS','AGESA.IS','AGHOL.IS','AHGAZ.IS','AKBNK.IS','AKCNS.IS','AKENR.IS','AKFGY.IS','AKFYE.IS','AKGRT.IS','AKMGY.IS','AKSEN.IS','AKSGY.IS','AKSUE.IS','AKYHO.IS','ALARK.IS','ALBRK.IS','ALCAR.IS','ALCTL.IS','ALFAS.IS','ALGYO.IS','ALKIM.IS','ANELE.IS','ANGEN.IS','ANHYT.IS','ANSGR.IS','ARASE.IS','ARCLK.IS','ARDYZ.IS','ARENA.IS','ARSAN.IS','ARZUM.IS','ASELS.IS','ASGYO.IS','ASTOR.IS','ASUZU.IS','ATAGY.IS','ATAKP.IS','ATATP.IS','ATEKS.IS','AVGYO.IS','AVHOL.IS','AVOD.IS','AVPGY.IS','AVTUR.IS','AYCES.IS','AYDEM.IS','AYGAZ.IS','AZTEK.IS','BAGFS.IS','BAKAB.IS','BANVT.IS','BARMA.IS','BASGZ.IS','BAYRK.IS','BEGYO.IS','BEYAZ.IS','BFREN.IS','BIENY.IS','BIGCH.IS','BIMAS.IS','BINHO.IS','BIOEN.IS','BIZIM.IS','BJKAS.IS','BLCYT.IS','BMSCH.IS','BMSTL.IS','BNTAS.IS','BOBET.IS','BORLS.IS','BOSSA.IS','BRISA.IS','BRKSN.IS','BRKVY.IS','BRLSM.IS','BRSAN.IS','BRYAT.IS','BTCIM.IS','BUCIM.IS','BURCE.IS','BURVA.IS','BVSAN.IS','BYDNR.IS','CANTE.IS','CATES.IS','CCOLA.IS','CELHA.IS','CEMAS.IS','CEMTS.IS','CEOEM.IS','CIMSA.IS','CLEBI.IS','CMBTN.IS','CONSE.IS','COSMO.IS','CRDFA.IS','CRFSA.IS','CUSAN.IS','CWENE.IS','DAGHL.IS','DAPGM.IS','DARDL.IS','DENGE.IS','DERHL.IS','DERIM.IS','DESPC.IS','DGATE.IS','DGGYO.IS','DGNMO.IS','DITAS.IS','DMRGD.IS','DMSAS.IS','DNISI.IS','DOBUR.IS','DOFER.IS','DOGUB.IS','DOHOL.IS','DOKTA.IS','DURDO.IS','DYOBY.IS','DZGYO.IS','EBEBK.IS','ECILC.IS','ECZYT.IS','EDATA.IS','EGEEN.IS','EGEPO.IS','EGGUB.IS','EGPRO.IS','EGSER.IS','EKGYO.IS','EKSUN.IS','ELITE.IS','EMKEL.IS','ENERY.IS','ENJSA.IS','ENKAI.IS','ENSRI.IS','EPLAS.IS','ERBOS.IS','EREGL.IS','ERSU.IS','ESCAR.IS','ESCOM.IS','ETILR.IS','EUHOL.IS','EUPWR.IS','EUREN.IS','EYGYO.IS','FADE.IS','FENER.IS','FMIZP.IS','FONET.IS','FORMT.IS','FORTE.IS','FRIGO.IS','FROTO.IS','FZLGY.IS','GARAN.IS','GARFA.IS','GEDIK.IS','GEDZA.IS','GENIL.IS','GENTS.IS','GEREL.IS','GESAN.IS','GIPTA.IS','GLBMD.IS','GLCVY.IS','GLRYH.IS','GLYHO.IS','GOKNR.IS','GOLTS.IS','GOODY.IS','GOZDE.IS','GRSEL.IS','GSDDE.IS','GSDHO.IS','GSRAY.IS','GUBRF.IS','GWIND.IS','GZNMI.IS','HALKB.IS','HATEK.IS','HATSN.IS','HDFGS.IS','HEDEF.IS','HEKTS.IS','HLGYO.IS','HTTBT.IS','HUBVC.IS','HUNER.IS','HURGZ.IS','ICBCT.IS','ICUGS.IS','IDGYO.IS','IEYHO.IS','IHAAS.IS','IHEVA.IS','IHGZT.IS','IHLAS.IS','IHLGM.IS','IHYAY.IS','IMASM.IS','INDES.IS','INFO.IS','INGRM.IS','INTEM.IS','INVEO.IS','INVES.IS','ISATR.IS','ISBTR.IS','ISCTR.IS','ISDMR.IS','ISFIN.IS','ISGSY.IS','ISGYO.IS','ISKPL.IS','ISMEN.IS','ISSEN.IS','IZENR.IS','IZFAS.IS','IZMDC.IS','JANTS.IS','KAPLM.IS','KAREL.IS','KARSN.IS','KARTN.IS','KATMR.IS','KAYSE.IS','KBORU.IS','KCAER.IS','KCHOL.IS','KERVT.IS','KFEIN.IS','KIMMR.IS','KLGYO.IS','KLKIM.IS','KLMSN.IS','KLRHO.IS','KLSER.IS','KLSYN.IS','KMPUR.IS','KNFRT.IS','KONKA.IS','KONTR.IS','KONYA.IS','KOPOL.IS','KORDS.IS','KRDMA.IS','KRDMB.IS','KRDMD.IS','KRGYO.IS','KRONT.IS','KRPLS.IS','KRSTL.IS','KRTEK.IS','KRVGD.IS','KTLEV.IS','KTSKR.IS','KUTPO.IS','KUYAS.IS','KZBGY.IS','KZGYO.IS','LIDER.IS','LIDFA.IS','LKMNH.IS','LRSHO.IS','LUKSK.IS','MAALT.IS','MACKO.IS','MAGEN.IS','MAKIM.IS','MAKTK.IS','MANAS.IS','MARBL.IS','MARKA.IS','MARTI.IS','MAVI.IS','MEDTR.IS','MEGAP.IS','MEGMT.IS','MEKAG.IS','MEPET.IS','MERCN.IS','MERIT.IS','MERKO.IS','METRO.IS','METUR.IS','MGROS.IS','MHRGY.IS','MIATK.IS','MNDRS.IS','MNDTR.IS','MOBTL.IS','MPARK.IS','MRGYO.IS','MRSHL.IS','MSGYO.IS','MTRKS.IS','MZHLD.IS','NATEN.IS','NETAS.IS','NIBAS.IS','NTGAZ.IS','NTHOL.IS','NUGYO.IS','NUHCM.IS','OBASE.IS','OFSYM.IS','ONCSM.IS','ORCAY.IS','OSMEN.IS','OSTIM.IS','OTKAR.IS','OYAKC.IS','OYLUM.IS','OYYAT.IS','OZGYO.IS','OZKGY.IS','OZRDN.IS','PAGYO.IS','PAMEL.IS','PAPIL.IS','PARSN.IS','PASEU.IS','PCILT.IS','PEKGY.IS','PENGD.IS','PENTA.IS','PETKM.IS','PETUN.IS','PGSUS.IS','PINSU.IS','PKART.IS','PKENT.IS','PLTUR.IS','PNLSN.IS','PNSUT.IS','POLHO.IS','POLTK.IS','PRDGS.IS','PRKAB.IS','PRZMA.IS','PSDTC.IS','PSGYO.IS','QUAGR.IS','RALYH.IS','RAYSG.IS','REEDR.IS','RNPOL.IS','RODRG.IS','RTALB.IS','RUBNS.IS','RYGYO.IS','RYSAS.IS','SAFKR.IS','SAHOL.IS','SAMAT.IS','SANEL.IS','SANFM.IS','SANKO.IS','SARKY.IS','SAYAS.IS','SDTTR.IS','SEGYO.IS','SEKFK.IS','SEKUR.IS','SELEC.IS','SELGD.IS','SELVA.IS','SEYKM.IS','SILVR.IS','SISE.IS','SKBNK.IS','SKTAS.IS','SKYMD.IS','SMART.IS','SMRTG.IS','SNGYO.IS','SNICA.IS','SOKE.IS','SOKM.IS','SONME.IS','SRVGY.IS','SUNTK.IS','SURGY.IS','SUWEN.IS','TABGD.IS','TARKM.IS','TATEN.IS','TATGD.IS','TAVHL.IS','TCELL.IS','TDGYO.IS','TEKTU.IS','TEZOL.IS','TGSAS.IS','THYAO.IS','TKFEN.IS','TKNSA.IS','TLMAN.IS','TMPOL.IS','TNZTP.IS','TOASO.IS','TRCAS.IS','TRGYO.IS','TRILC.IS','TSGYO.IS','TSPOR.IS','TTKOM.IS','TTRAK.IS','TUCLK.IS','TUKAS.IS','TUPRS.IS','TUREX.IS','TURGG.IS','TURSG.IS','ULKER.IS','ULUFA.IS','ULUSE.IS','ULUUN.IS','UNLU.IS','VAKBN.IS','VAKFN.IS','VAKKO.IS','VANGD.IS','VBTYZ.IS','VERTU.IS','VERUS.IS','VESBE.IS','VESTL.IS','VKGYO.IS','VKING.IS','VRGYO.IS','YATAS.IS','YAYLA.IS','YEOTK.IS','YESIL.IS','YGGYO.IS','YKBNK.IS','YKSLN.IS','YUNSA.IS','YYAPI.IS','YYLGD.IS','ZEDUR.IS','ZOREN.IS','ZRGYO.IS','YGYO.IS','GMTAS.IS','XU100.IS','XU030.IS',]
 bist100 = ['AEFES.IS','AGHOL.IS','AHGAZ.IS','AKBNK.IS','AKCNS.IS','AKFGY.IS','AKFYE.IS','AKSA.IS','AKSEN.IS','ALARK.IS','ALBRK.IS','ALFAS.IS','ARCLK.IS','ASELS.IS','ASTOR.IS','BERA.IS','BIENY.IS','BIMAS.IS','BRSAN.IS','BRYAT.IS','BUCIM.IS','CANTE.IS','CCOLA.IS','CIMSA.IS','CWENE.IS','DOAS.IS','DOHOL.IS','ECILC.IS','ECZYT.IS','EGEEN.IS','ENJSA.IS','ENKAI.IS','EREGL.IS','EUPWR.IS','EUREN.IS','FROTO.IS','GARAN.IS','GENIL.IS','GESAN.IS','GLYHO.IS','GUBRF.IS','HALKB.IS','HEKTS.IS','IMASM.IS','IPEKE.IS','ISCTR.IS','ISDMR.IS','ISMEN.IS','IZMDC.IS','KARSN.IS','KAYSE.IS','KCAER.IS','KCHOL.IS','KMPUR.IS','KONTR.IS','KONYA.IS','KORDS.IS','KOZAA.IS','KOZAL.IS','KRDMD.IS','KZBGY.IS','MAVI.IS','MGROS.IS','MIATK.IS','ODAS.IS','OTKAR.IS','OYAKC.IS','PENTA.IS','PETKM.IS','PGSUS.IS','QUAGR.IS','SAHOL.IS','SASA.IS','SISE.IS','SKBNK.IS','SMRTG.IS','SOKM.IS','TAVHL.IS','TCELL.IS','THYAO.IS','TKFEN.IS','TOASO.IS','TSKB.IS','TTKOM.IS','TTRAK.IS','TUKAS.IS','TUPRS.IS','ULKER.IS','VAKBN.IS','VESBE.IS','YEOTK.IS','YKBNK.IS','YYLGD.IS','ZOREN.IS']
 global_Markets = ["^DJI","^GSPC","^IXIC","^RUT","^VIX","^GSPTSE","^BVSP","^MXX","URTH","^GDAXI","^FTSE","^FCHI","^STOXX50E","^AEX","^IBEX","FTSEMIB.MI","^SSMI","^PSI20","BEL20.BR","^ATX","^OMXS30","^OMXC25","IMOEX.ME","RTSI.ME","^WIG20","BUX.BD","XU100.IS","TA35.TA","TASI.SR","^N225","^AXJO","^NZ50","000001.SS","399001.SZ","XIN9.DE","^DJSH","^HSI","^TWII","^SET.BK","^KS11","^JKSE","^NSEI","^BSESN","^PSI","KSE100.KA","^VN30" ]
-US_stocks = ["NVDA","WBD","TSLA","NU","F","INTC","HBAN","BAC","AAPL","AMD","CVX","PLTR","AMZN","PFE","IVZ","VALE","SLB","GOOGL","BBD","KEY","XOM","SMCI","ITUB","PBR","SNAP","T","ABEV","BABA","CSX","PCG","BCS","SCHW","RF","CNC","WFC","STLA","MSFT","GOOG","VZ","NOK","AMCR","ABT","MU","MSTR","KMI","KHC","CCL","MRVL","C","TSM","CSCO","USB","AVGO","UNH","HAL","KO","CMCSA","WMT","UBER","HPE","KGC","PEP","TFC","FCX","NEM","ET","META","KVUE","JPM","TEVA","BKR","MRK","OXY","MMM","RKT","PINS","TTD","IPG","DELL","XYZ","BMY","CVE","NFLX","PDD","MDLZ","CNH","VTRS","JNJ","TME","INFY","CAG","ORCL","CMG","DIS","CFG","HLN","AES","CPRT","DOW","PYPL","DAL","NCLH","NKE","GE","CVS","BSX","RBLX","LYG","ELV","LUV","LRCX","NVO","NEE","COP","WMB","MRNA","CTRA","UAL","FAST","KDP","FITB","QCOM","TOST","PG","CPNG","B","PPL","OWL","HPQ","PBR-A","STM","NDAQ","DVN","FNF","PARA","EBAY","GILD","ENPH","ACGL","GM","VST","MDT","RDDT","CHWY","CRM","APH","BP","ALB","VICI","GSK","NSC","EQT","BTI","ANET","BA","RTX","EOG","UNP","VRT","CNP","ON","MO","EXC","BK","CP","IBN","MS","AXP","LNG","D","MNST","XPEV","APA","KR","ASX","ABBV","TGT","SHEL","MCHP","CCI","WBA","PPG","SBUX","HST","DHI","WY","CARR","CL","CNQ","V","CTSH","ARM","SAN","APP","WIT","TROW","EW","OMC","AMAT","ZTS","PM","CZR","CCJ","AFL","UPS","OKE","TXN","MOS","PLD","IBM","DOC","AEP","IOT","SE","TEL","GIS","HUM","TSCO","WDC","IR","DHR","NET","ACN","SO","BEKE","O","BX","MGM","BRK-B","ORLY","FI","BEN","RBRK","NI","ES","EA","BF-B","KMB","CRH","TMUS","BAX","WRB","GEN","EPD","LVS","PCAR","CTVA","SNPS","TAK","FTNT","SRE","HON","VG","DDOG","MOH","DLTR","BBY","CEG","MMC","MCD","BDX","KIM","TJX","UMC","DD","XEL","VLO","DECK","NRG","DASH","TPR","SNOW","GEHC","HD","LLY","CPB","TWLO","STX","BHP","PSX","EQNR","SYF","JCI","SU","TTWO","HES","ACM","HRL","KKR","COF","NWG","SONY","PEG","EIX","AZN","ADM","ABNB","SW","PANW","CB","MUFG","AIG","PGR","IP","ONON","ETN","HDB","APO","STT","CRWD","CNI","RIO","MFG","KMX","TU","CVNA","ADI","AMH","FTV","FE","DG","NWSA","ROST","MTCH","LNT","TMO","ENB","USFD","MPC","DXCM","FSLR","WDAY","MA","LW","DOV","ASML","NTR","AS","ING","LULU","PNC","NVS","CTAS","CAT","TER","ETR","GLW","NXPI","PAYX","AOS","GFI","ICE","APTV","TECK","DUK","INVH","ADBE","ADSK","VTR","LOW","ARES","HSY","FANG","FIS","SMFG","AU","HLT","SYY","IQV","GPN","CF","EL","SJM","LIN","HWM","EXE","TRP","LYB","EC","LEN","OTIS","SHW","CME","FOXA","MFC","SPOT","VIK","BALL","K","CBOE","COST","APD","BCE","CI","MET","WYNN","GEV","WEC","CSGP","CAH","WM","CHD","EVRG","BRO","SWK","AMGN","YUMC","MCO","PNR","BN","PHM","AWK","GS","EMR","FLUT","STZ","ISRG","TAP","EBR","UDR","AEM","COO","VLTO","THC","LMT","HAS","MDB","ZS","TSN","ED","BLDR","EXPD","TECH","EQR","SWKS","FDX","VEEV","NTAP","ALC","DT","DB","STE","SYK","DTE","TRV","LKQ","CLX","GFS","MAS","ADP","JBHT","GD","TD","EQH","ALL","PSTG","HIG","QSR","YUM","TRMB","VIV","HSBC","CMS","HOLX","CRBG","TEAM","BUD","RCI","WELL","DGX","INCY","BAM","ODFL","MAR","CUK","FERG","IFF","VRSK","BIIB","A","BG","INTU","TRU","NUE","WST","ARE","LYV","RCL","AKAM","GPC","TXT","LHX","TRGP","ECL","UBS","MKC","UL","ROL","DLR","CDNS","AEE","NTRS","BXP","MPLX","YPF","RSG","SPGI","IT","PRU","ZBH","REGN","TT","DAY","JBL","GDDY","AMT","CHRW","BBVA","MT","TS","MTB","CCEP","BJ","AMX","HSIC","BSBR","DPZ","NOW","EXPE","RJF","WPM","CHTR","AER","BAH","WCN","WSM","AON","SUI","DE","PWR","DRI","COR","WES","TTE","RBA","HCA","ITW","SAP","PNW","AME","PSA","RVTY","CBRE","BLK","REG","VRTX","XYL","TKO","SPG","L","BNS","ALLE","STLD","SCCO","DEO","CMI","PKG","EMN","RMD","PTC","NOC","PBA","TDY","PFG","IRM","GNRC","CDW","PHG","RELX","WAB","GRMN","VMC","BURL","FOX","NWS","EFX","EXR","RYAN","ATO","GWW","CM","MSI","MAA","WAT","RPM","DVA","WTW","KEYS","CINF","SBS","AJG","POOL","KLAC","ROK","GFL","HUBS","NGG","CPT","IEX","ROP","UHS","MHK","EQIX","SNA","PUK","FRT","HMC","FFIV","GL","MSCI","SOLV","J","LH","GGG","FTS","SLF","HUBB","BEP","HII","AVY","WDS","KEP","GWRE","RL","TEF","BIP","RS","MCK","BR","CRL","PH","PAYC","RY","AVB","ALGN","LDOS","NDSN","EPAM","FNV","TLK","BMO","MPWR","CPAY","JKHY","NMR","ZBRA","FDS","ULTA","EME","MLM","AMP","FMX","AXON","ULS","SBAC","URI","RACE","MKTX","AIZ","VRSN","BCH","PODD","TM","HEI","IDXX","MELI","CSL","ESS","BAP","GIB","EG","KB","TYL","WSO","FIX","TDG","LII","ERIE","E","FMS","KOF","CW","IHG","FICO","CQP","HEI-A","CHT","BF-A","BKNG","TPL","SHG","IX","AZO","UI","MTD","MKL","LEN-B","MKC-V","NVR","BBDO","BRK-A","WSO-B"]
-portfolio = ["MGROS.IS","KCHOL.IS","THYAO.IS","KRDMD.IS","CCOLA.IS","BRSAN.IS","ZOREN.IS","TTRAK.IS","AEFES.IS","FROTO.IS","ALCAR.IS"] 
+
+
 
 ################################################CLASSIC STOCK DATA ANALYSIS With Ichımochu###################################################
 #bist = endeks
-def classic_analysis(Stock_List):
+def backtest(startDate, endDate, bist):
     stock_dict = {}
     stock_data = []
     stock_number = 0
-    for stocks in Stock_List:
+    for stocks in bist:
         #time.sleep(1)
         try:
             ticker = yf.Ticker(stocks)
             stock_info = ticker.info
-            data = {key: stock_info.get(key, None) for key in ["symbol", "priceToBook", "currentPrice", "targetHighPrice", "targetLowPrice", "targetMeanPrice", "targetMedianPrice",
+            data = {key: stock_info.get(key, None) for key in ["symbol", "priceToBook","currentPrice", "targetHighPrice", "targetLowPrice", "targetMeanPrice", "targetMedianPrice",
                                                                 "bookValue", "open", "dayLow", "dayHigh", "recommendationKey", 'fiftyTwoWeekLow', 'fiftyTwoWeekHigh']}
-            historical_data = ticker.history(period="1y")  # Get historical data for the stock
+            #historical_data = ticker.history(period="1y")  # Get historical data for the stock
+            historical_data = ticker.history(start=startDate, end=endDate) 
             stock_dict[stocks] = historical_data
             if historical_data.empty:
                 print(f"No historical data for {stocks}, skipping...")
                 continue  # Skip this ticker if no historical data is found
             stock_number = 1 + stock_number
-            print(f"\r{stock_number}/{len(Stock_List)} Downloaded {stocks} data", end='', flush=True)
+            print(f"\r{stock_number}/{len(bist)} Downloaded {stocks} data", end='', flush=True)
 
             historical_data['RSI'] = ta.rsi(historical_data['Close'], length=14)  # Calculate RSI (Relative Strength Index)
             adx_data = ta.adx(historical_data['High'], historical_data['Low'], historical_data['Close'], length=14)  # Calculate ADX (Average Directional Index)
@@ -131,6 +132,7 @@ def classic_analysis(Stock_List):
 
     # Create a DataFrame from the stock data
     df = pd.DataFrame(stock_data)
+    #df["currentPrice"] = historical_data["close"]
     df["time"] = datetime.now()
     df["YF%"] = ((df["currentPrice"] - df["fiftyTwoWeekLow"])/(df["fiftyTwoWeekHigh"]-df["fiftyTwoWeekLow"]))*100 #Check the differences
     df["MACDAS-dif"] = df["MACDAS"] - df["MACDAS_Signal"] 
@@ -155,16 +157,18 @@ def classic_analysis(Stock_List):
 
     #pd.set_option('display.float_format', '{:.2f}'.format)
     print("\nDownload process is done!")
-    now = datetime.now().strftime("%d_%m_%y")
-    #formatted_time = now.strftime("%d_%m_%y")
-    df2.to_csv(f"CSVs/Results last.csv")
-    if Stock_List == bist:
-        df2.to_csv(f"CSVs/Results/bist_{now}.csv")
-    elif Stock_List == US_stocks:
-        df2.to_csv(f"CSVs/Results/US_stocks_{now}.csv")
-    elif Stock_List == portfolio:
-        df2.to_csv(f"CSVs/Results/portfolio_{now}.csv")    
-    else:
-        print("Saving Process Failed!")
+    now = datetime.now()  # This part will copy for our sell lists. Especially further analysis.
+    formatted_time = now.strftime("%m-%d_%H-%M-%S")
+    #df2.to_csv(f"CSVs/Backtesting/{bist}.csv",mode='a', header=True, index=True)
+    file_path = f"CSVs/Backtesting/{bist}.csv"
+    file_exists = os.path.isfile(file_path)
+
+    df2.to_csv(file_path, mode='a', header=not file_exists, index=False)
     print("Stock Market Data Processing is Done!")
-#classic_analysis(portfolio)
+    print(df2)
+
+endDate   = "2025-03-20"
+startDate = "2024-06-25"
+stock = ["KCHOL.IS"]
+
+backtest(startDate=startDate,endDate=endDate,bist=stock)
